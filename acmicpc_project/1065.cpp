38174135249279beedcd0ee77990a35483cfa9b0
tornadoblot <newclearplant@gmail.com>
2019-10-04 13:44:58 +0900

    함수 푸는중
    학교 나올땐 하루 한번 푸시하자

---
+++ acmicpc_project/1065.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int arr[1001];
+
+bool d(int n)
+{
+	int tmp, ori = n, cha;
+	bool chk = true;
+
+	for (; ori > 99; ori /= 10)
+	{
+
+
+	}
+
+
+	return chk;
+}
+
+int main()
+{
+
+
+	return 0;
+}
--- acmicpc_project/4673.cpp
+++ acmicpc_project/4673.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int arr[10001];
+
+int d(int d)
+{
+	int sum = d, tmp = d;
+	
+	for (; tmp > 0;tmp /= 10)
+	{
+		sum += tmp % 10;
+	}
+
+	return sum;
+}
+
 int main()
 {
-	
+	for (int i = 1; i <= 10000; i++)
+	{
+		arr[d(i)] = 1;
+		if (!arr[i]) cout << i << '\n';
+	}
 
 	return 0;
 }
