58883890a0f083b1bb13d43424c359ace441b3ac
tornadoblot <newclearplant@gmail.com>
2019-10-08 13:44:36 +0900

    이게 얼마만이야
    내가 포기하는걸 포기해

--- acmicpc_project/1065.cpp
+++ acmicpc_project/1065.cpp
@@ -2,26 +2,41 @@
 #include <math.h>
 using namespace std;
 
-int arr[1001];
-
 bool d(int n)
 {
 	int tmp, ori = n, cha;
 	bool chk = true;
 
-	for (; ori > 99; ori /= 10)
+	tmp = ori % 10;
+	ori /= 10;
+	cha = tmp - ori % 10;
+
+	while (ori > 9)
 	{
-
-
+		tmp = ori % 10;
+		ori /= 10;
+		if (tmp - ori % 10 != cha)
+		{
+			chk = false;
+			break;
+		}
 	}
-
 
 	return chk;
 }
 
 int main()
 {
+	int n, cnt = 0;
+	
+	cin >> n;
 
+	for (int i = 1; i <= n; i++)
+	{
+		if (d(i)) cnt++;
+	}
+
+	cout << cnt;
 
 	return 0;
 }
